15d191570479cba4f8be19b57fabed7f8e8d5f9b
Jueruchen <101071327+Jueruchen@users.noreply.github.com>
2022-06-06 14:55:36 -0400

    Jun 6 Updates
    All the latest updates including:
    - plutopay project details
    - links update

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,24 @@ function menuToggle() {
     x.className = 'navtoggle';
   }
 }
+
+// $('.popover1').on({
+//     mousemove: function(e) {
+//         $(this).next('#big').css({
+//             top: e.pageY - 260 - 25, // height of image and title area plus some
+//             left: e.pageX + -120
+//         });
+//     },
+//     mouseenter: function() {
+//         var $big = $('<img />', {'class': 'big_img1', src: this.src}),
+//             $title = $('<div class="title"/>').html(this.title),
+//             $frame = $('<div id="big" />');
+//
+//         $frame.append($big).append($title);
+//
+//         $(this).after($frame);
+//     },
+//     mouseleave: function() {
+//         $('#big').remove();
+//     }
+// });
